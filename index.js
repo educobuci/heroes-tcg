eae5a30e002235c140fb9a99879c34682ef806ff
Eduardo Cobuci <educobuci@gmail.com>
2017-02-04 01:14:36 -0200

    Messages support

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const app = require('express')();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
 });
 
 http.listen(3000, () => {
